4c42226c8ba57b4b13d513116127e2191beac7db
Amanda <amandalragone@gmail.com>
2019-03-29 12:39:38 -0500

    Added the on click function for attack.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,25 +4,26 @@ $(document).ready(function(){
     var fighter
     var enemy
     var isThereADefender
+    
 
     //Each character needs a set of values: Health Points, Attack Power and Counter Attack Power
     var fighterPower = {
-        $("#martell"): {
+        martell: {
             healthPoints: 100,
             attackPower: 9,
             counterAttackPower: 23,
         },
-        $("#snow"): {
+        snow: {
             healthPoints: 110,
             attackPower: 8,
             counterAttackPower: 20,
         },
-        $("#mountain"): {
+        mountain: {
             healthPoints: 120,
             attackPower: 7,
             counterAttackPower: 18,
         }, 
-        $("#night"): {
+        night: {
             healthPoints: 130,
             attackPower: 6,
             counterAttackPower: 15,
@@ -34,14 +35,16 @@ $(document).ready(function(){
     // User will click an image to pick fighter.
 
     $(".character").on("click", function(){
-        fighter = ($(this));
+        fighter = $(this);
+        const fighterID = fighter.find("img").attr("id");
 
         // Enemies will move to a different area of the page.
         $(this).siblings()
         .addClass("enemiesAvailable")
         .appendTo("#enemy")
         .on("click", function(){ // User will click on an enemy to choose it.
-            enemy = ($(this));
+            enemy = $(this);
+            const enemyID = enemy.find("img").attr("id");
 
             $("#defender").html(this);
             isThereADefender = true;
@@ -50,6 +53,42 @@ $(document).ready(function(){
                 $(".enemiesAvailable").off("click");
             };
         });
+    });
+
+
+    // $(".character").on("click", function(){
+
+    //     if (isThereAFighter === false) {
+    //         fighter = $(this);
+    //         const fighterID = fighter.find("img").attr("id");
+    //         isThereAFighter === true
+    
+    //         // Enemies will move to a different area of the page.
+    //         $(this).siblings()
+    //         .addClass("enemiesAvailable")
+    //         .appendTo("#enemy");
+    //     } else if (isThereAFighter === true) {
+    //         enemy = $(this);
+    //         console.log("hello");
+    //         const enemyID = enemy.find("img").attr("id");
+
+    //         $(this).appendTo("#defender");
+
+    //         isThereADefender = true; 
+    //     } else if (isThereADefender === true){
+    //         $(".character").off("click");
+    //     }
+        
+    // });
+
+    //User will play the game when clicking the attack button.
+
+    $("#attack").on("click", function(){
+        const enemyID = enemy.find("img").attr("id");
+        const fighterID = fighter.find("img").attr("id");
+
+        console.log(enemyID + "attacked by" + fighterID);
+        console.log("new fighter health", fighterPower[enemyID].healthPoints - fighterPower[fighterID].attackPower);
     });
 
     
